bfe8866183dd5f7b0bbd0a89c62e891b4dbf01da
Michael Storgaard <git@mex.io>
2015-05-24 14:35:50 +0200

    Added Jimmy Fallon support and fixed IMDb fetcher

--- Renamer.java
+++ Renamer.java
@@ -7,18 +7,18 @@ import java.awt.event.*;
 import javax.swing.*;
 
 class Renamer {
-	
+
 	private String url = "";
 	private ArrayList<String[]> shows = new ArrayList<String[]>();
 	private HashMap<String, String[]> data = new HashMap<String, String[]>();
 	private JTextArea textArea;
-	
+
 	public Renamer() {
 		this.setupLayout();
 		this.fetchShowInfo();
 		this.runScript();
 	}
-	
+
 	private void setupLayout() {
 		JFrame frame = new JFrame();
 		frame.addWindowListener(new WindowAdapter() {
@@ -34,16 +34,16 @@ class Renamer {
 		frame.setSize(640, 480);
 		frame.setVisible(true);
 	}
-	
+
 	private void appendOutput(String output) {
 		this.textArea.append("\n" + output);
 		this.textArea.setCaretPosition(this.textArea.getDocument().getLength());
 	}
-	
+
 	private void fetchShowInfo() {
 		try {
 			this.appendOutput("Downloading show info...");
-			URL url = new URL(url);
+			URL url = new URL(this.url);
 			InputStream is = url.openStream();
 			Scanner scanner = new Scanner(is).useDelimiter("\\A");
 			String body = scanner.hasNext() ? scanner.next() : "";
@@ -55,7 +55,7 @@ class Renamer {
 			this.appendOutput("Error occurred: " + ex.getMessage());
 		}
 	}
-	
+
 	private void runScript() {
 		String dirName = "./";
 		File dir = new File(dirName);
@@ -63,21 +63,32 @@ class Renamer {
 		for (int i = 0; i < files.length; i++) {
 			this.appendOutput("Processing '" + files[i].getName() + "'...");
 			String fileName = files[i].getName();
-			if (fileName.length() > 14 && fileName.substring(0, 14).equals("The.Daily.Show")) {
+			if (fileName.length() > 14 && fileName.substring(0, 14).toLowerCase().equals("the.daily.show")) {
 				String date = fileName.substring(15, 25).replace(".", "-");
-				String title = fileName.substring(26).split(".HDTV")[0].replace(".", " ");
+				String title = fileName.substring(26).toLowerCase().split(".hdtv")[0].replace(".", " ");
 				try {
-					String newName = "The Daily Show - " + date + " - " + title + "." + this.getExt(files[i].getName());
+					String newName = "The Daily Show - " + date + " - " + this.capitalizeString(title) + "." + this.getExt(files[i].getName());
 					this.appendOutput("Renaming to '" + newName + "'...");
 					this.renameFile(files[i], newName);
 				} catch (Exception e) {
 					this.appendOutput("Error occurred: " + e.getMessage());
 				}
-			} else if (fileName.length() > 15 && fileName.substring(0, 15).equals("David.Letterman")) {
+			} else if (fileName.length() > 15 && fileName.substring(0, 15).toLowerCase().equals("david.letterman")) {
 				String date = fileName.substring(16, 26).replace(".", "-");
-				String title = fileName.substring(27).split(".HDTV")[0].replace(".", " ");
+				String title = fileName.substring(27).toLowerCase().split(".hdtv")[0].replace(".", " ");
 				try {
-					String newName = "David Letterman - " + date + " - " + title + "." + this.getExt(files[i].getName());
+					String newName = "David Letterman - " + date + " - " + this.capitalizeString(title) + "." + this.getExt(files[i].getName());
+					this.appendOutput("Renaming to '" + newName + "'...");
+					this.renameFile(files[i], newName);
+				} catch (Exception e) {
+					this.appendOutput("Error occurred: " + e.getMessage());
+				}
+			} else if (fileName.length() > 12 && fileName.substring(0, 12).toLowerCase().equals("jimmy.fallon")) {
+				String date = fileName.substring(13, 23).replace(".", "-");
+				String title = fileName.substring(24).toLowerCase().split(".hdtv")[0].replace(".", " ");
+				this.appendOutput(fileName.substring(24).toLowerCase().split(".hdtv")[0] + ": " + fileName.substring(24).toLowerCase().split(".hdtv")[1]);
+				try {
+					String newName = "Jimmy Fallon - " + date + " - " + this.capitalizeString(title) + "." + this.getExt(files[i].getName());
 					this.appendOutput("Renaming to '" + newName + "'...");
 					this.renameFile(files[i], newName);
 				} catch (Exception e) {
@@ -106,15 +117,15 @@ class Renamer {
 		}
 		this.appendOutput("Finished");
 	}
-	
+
 	private String simplifyName(String fileName) {
 		return fileName.toLowerCase().replace(".", " ");
 	}
-	
+
 	private String getExt(String fileName) {
 		return fileName.substring(fileName.lastIndexOf(".")+1);
 	}
-	
+
 	private String[] identifyShow(String name) {
 		String[] info = {};
 		for (String[] show : this.shows) {
@@ -124,7 +135,7 @@ class Renamer {
 		}
 		return info;
 	}
-	
+
 	private String fetchData(String imdb, String season, String episode) {
 		int s = Integer.parseInt(season);
 		int e = Integer.parseInt(episode);
@@ -154,33 +165,47 @@ class Renamer {
 		}
 		return "";
 	}
-	
+
 	private String fetchTitle(String body, int s, int e) {
 		String[] lines = body.split("\n");
 		for (int i = 0; i < lines.length; i++) {
 			if (lines[i].matches("<div>S" + s + ", Ep" + e + "</div>")) {
-				String se = "<img width=\"120\" class=\"zero-z-index\" alt=\"(.*?)\" src=\"(.*?)\">";
+				String se = "<img(.*?)alt=\"(.*?)\" src=\"(.*?)\">";
 				if (!Pattern.matches(se, lines[i-1])) return "";
 				Pattern p = Pattern.compile(se);
 				Matcher m = p.matcher(lines[i-1]);
 				m.find();
-				return " - " + this.cleanTitle(m.group(1));
+				return " - " + this.cleanTitle(m.group(2));
 			}
 		}
 		return "";
 	}
-	
+
 	private String leadingZero(String str) {
 		if (str.length() == 1) {
 			return "0" + str;
 		}
 		return str;
 	}
-	
+
 	private String cleanTitle(String title) {
 		return title.replace(":", "").replace("\\", "").replace("/", "").replace("*", "").replace("?", "").replace("\"", "").replace("<", "").replace(">", "").replace("|", "");
 	}
-	
+
+	private String capitalizeString(String string) {
+		char[] chars = string.toLowerCase().toCharArray();
+		boolean found = false;
+		for (int i = 0; i < chars.length; i++) {
+			if (!found && Character.isLetter(chars[i])) {
+				chars[i] = Character.toUpperCase(chars[i]);
+				found = true;
+			} else if (Character.isWhitespace(chars[i]) || chars[i]=='.' || chars[i]=='\'') { // You can add other chars here
+				found = false;
+			}
+		}
+		return String.valueOf(chars);
+	}
+
 	private void renameFile(File file, String name) throws IOException {
 		File tempFile = new File(file.getParent() + "/temp-" + name);
 		if(tempFile.exists()) {
@@ -190,7 +215,7 @@ class Renamer {
 		if (!success) {
 			throw new IOException("File could not be renamed");
 		}
-		
+
 		File file2 = new File(file.getParent() + "/" + name);
 		if(file2.exists()) {
 			return;
@@ -200,7 +225,7 @@ class Renamer {
 			throw new IOException("File could not be renamed");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Renamer r = new Renamer();
 	}
